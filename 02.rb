1eb2340d0ef8d2d44049e6dde3f229b25d510d86
Anthony Tonatihu Rosales Ceja <arosalesceja1997@gmail.com>
2020-06-02 13:44:29 -0500

    sesion-03 - tercera parte

--- 02.rb
+++ 02.rb
@@ -1,35 +1,5 @@
 # frozen_string_literal: true
 
-# # frozen_string_literal: true
-
-# # print 'tamaño de tablero: '
-# board_size = 10
-
-# # puts board_size
-# board_size = board_size.to_i # transforma string a integer
-# # board_size es 0 si no se recibe un numero valido de entrada
-# board = []
-
-# # llenar tablero
-# board_size.times do
-#   column = []
-#   board_size.times do |i|
-#     column.push i
-#   end
-#   board.push column
-# end
-
-# # imprimir matriz
-# board.each do |column|
-#   column.each do |card|
-#     print card
-#     print ' ' # print no imprime un salto de linea al final del output
-#   end
-#   puts '' # salto de linea
-# end
-
-# !/usr/bin/env ruby
-
 require 'gtk3'
 
 app = Gtk::Application.new 'com.iridakos.gtk-todo', :flags_none
